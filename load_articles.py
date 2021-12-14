ac250ba30bae2da51b517d63c640b7d5b5f8c1aa
SophieLiuYQ <90285787+SophieLiuYQ@users.noreply.github.com>
2021-12-14 00:44:35 -0500

    Update load_articles.py
    fixed time formatting

--- load_articles.py
+++ load_articles.py
@@ -16,49 +16,50 @@ from dateutil.parser import parse
 import logging
 
 
-def load_articles(day, time_num, STOCK_TAGS):
-    stock_data = {}
+def load_articles(day, time_num):
     # directory is './data/data_{}'
     for ticker in STOCK_TAGS:
+        # start = time.time()
 
         # Try to open the file for that stock from the given directory
-        logging.debug("Starting to load articles for: " + ticker)
+        logging.debug('Starting to load articles for: ' + ticker)
         try:
-            file = open("./data/data_{}.csv".format(ticker), "r")
+            file = open('./data/data_{}.csv'.format(ticker), 'r')
         except IOError as error:
-            logging.warning(
-                "Could not load articles for: " + ticker + ", Error: " + str(error)
-            )
+            logging.warning('Could not load articles for: ' + ticker + ', Error: ' + str(error))
             continue
 
         # skip the first line as it is the header
         file.readline()
         # Prepare variables to save the article data
         stock_data[ticker] = []
-
-        need_time = (time_num * 2 + 8, time_num * 2 + 10)
+        need_time = (time_num*2 + 8, time_num*2 + 10)
         for line in file:
             data = line.strip().rsplit(",", 1)
-
             # data[0] is the news header and data[1] is the datetime
             date_time = data[1]
-            # parse the second element to only include the date part, i.e., date = 12/13/21
-            date = str(parse(date_time.split()[0])).split()[0]
-            # parse the second element to only include the time part, i.e., localtime = 15:15
-            localtime = date_time.split()[1]
-            # parse the localtime to only include the hrs, i.e., hour = 15
+            date_time = date_time.split()
+            if len(date_time) == 0:
+                continue
+            date = (str(parse(date_time[0])).split())[0]
+            localtime = "00:00" if (len(date_time) == 1) else date_time[1]
             hour = int(localtime.split(":")[0])
-
-            # if we load articles for 14-16,we actually load all the aricles after 14 pm
-            # , because the data is used for tomorrow morning
+            # print(hour)
             if date == day:
+                print(data[0], hour, need_time)
                 if time_num != 3:
                     if hour >= need_time[0] and hour < need_time[1]:
                         stock_data[ticker].append(data[0])
                 else:
+                    # if we load articles for 14-16,we actually load all the aricles after 14 pm
+                    # , because the data is used for tomorrow morning
                     if hour >= need_time[0]:
                         stock_data[ticker].append(data[0])
-        # print(current header)
-        file.close()
+        # print(current_article)
+        print(stock_data[ticker])
+        # if len(stock_data[ticker]) < SUCCESS_THREASHOLD:
+        #     logging.error('Could not load the threshold (' + str(
+        #         SUCCESS_THREASHOLD) + ') number of articles. Either not saved or another error occured.')
+        #     return False
     print(stock_data)
-    return stock_data
+    return True
