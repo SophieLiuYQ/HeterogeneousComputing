2af89b6dabd483bbaf278c18d43ef95b26a951b9
Justin Szaday <jjszaday@gmail.com>
2023-01-18 23:25:52 -0800

    massive cleanup

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ sudo -u ec2-user /bin/bash << EOF
 echo "executing as..."
 whoami
 source "/home/ec2-user/.bashrc"
+cd "/home/ec2-user/HeterogeneousComputing"
 python3 "/home/ec2-user/HeterogeneousComputing/CPUPullPrice.py"
 EOF
